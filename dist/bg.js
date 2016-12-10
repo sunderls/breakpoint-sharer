073a0a1267e759d094c305170302cfd84b65e9f9
sunderls <sunderls@126.com>
2016-12-11 01:39:35 +0900

    click to show popup

--- dist/bg.js
+++ dist/bg.js
@@ -1,3 +1,13 @@
+/**
+ * click button to enable debugger
+ */
 chrome.browserAction.onClicked.addListener((tab) => {
-	alert('clicked');
+	chrome.debugger.attach({tabId: tab.id}, '1.0', () => {
+		chrome.windows.create({
+			url: `debugger.html?tabId=${tab.id}`,
+			type: 'popup',
+			width: 600,
+			height: 700
+		});
+	});
 });
